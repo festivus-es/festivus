e0b17289186b2d17de160a9a50d3e9ad5680e8a9
alex <alex@pdp7.net>
2023-04-10 16:24:58 +0200

    Add index
    Fixes #14

--- festivus.py
+++ festivus.py
@@ -26,12 +26,41 @@ def get_calendar(location, paths):
 
 def generate_calendars():
     calendars = find_calendars()
+    base = pathlib.Path("generated") 
     for calendar in calendars:
-        path = pathlib.Path("generated") / pathlib.Path(*calendar.location)
+        path = base / pathlib.Path(*calendar.location)
         path.mkdir(parents=True, exist_ok=True)
         file = path / "festivus.ics"
         with open(file, "w") as f:
             f.write(calendar.as_ical().serialize())
+
+    with open(base / "index.html", "w") as f:
+        f.write("""
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        </head>
+        <body>
+        <ul>
+        """)
+        prev_last_year = None
+        for calendar in sorted(calendars, key=lambda c: (-c.years()[-1], c.location[::-1])):
+            last_year = calendar.years()[-1]
+            if last_year != prev_last_year:
+                if prev_last_year:
+                    f.write("</ul></li>")
+                f.write(f"<li>{last_year}<ul>")
+            url = "/".join(list(calendar.location) + ["festivus.ics"])
+            f.write(f"""
+            <li><a href="{url}">{calendar.location[2]}</a> {calendar.location[1]}, {calendar.location[0]} {calendar.years()}</li>
+            """)
+            prev_last_year = last_year
+        f.write("""
+        </ul>
+        </li>
+        </ul>
+        </body>
+        """)
 
 
 class Calendar:
@@ -60,6 +89,8 @@ class Calendar:
             ical.events.add(event)
         return ical
 
+    def years(self):
+        return sorted(set([d.date.year for d in self.days]))
 
 class Day:
     def __init__(self, line, source):
