1ae4f0ac745febf2acfef889d6ffa57de6d7dc2d
alex <alex@pdp7.net>
2025-09-30 19:40:16 +0200

    Improve docs

--- festivus.py
+++ festivus.py
@@ -42,7 +42,7 @@ def generate_calendars():
         <meta name="viewport" content="width=device-width, initial-scale=1">
         </head>
         <body>
-        <p>Contribute data at <a href="https://github.com/festivus-es/festivus">github.com/festivus-es/festivus</a>.</p>
+        <p>Visit <a href="https://github.com/festivus-es/festivus">github.com/festivus-es/festivus</a> to contribute data and learn more about this project.</p>
         <ul>
         """
         )
